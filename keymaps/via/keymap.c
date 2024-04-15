92ead37e1f3d3e8d9f731e14a16043afe35bf0fe
Kira-god <kirayuzo189@gmail.com>
2024-04-15 17:35:09 +0630

    first commit

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -58,3 +58,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LGUI,                            _______,                   _______, _______             
     ),
 };
+void keyboard_post_init_user(void) {
+    // Initialize RGB to static black
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_BLACK);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+void housekeeping_task_user(void) {
+    rgblight_setrgb_at(255, 0, 0, 0);
+}
